309652c0d247e6d7261e255d7ee233043fc63f5c
jeferson350Z <jefersonjd045@gmail.com>
2026-09-21 10:20:05 -0300

    feat: criar estrutura inicial de navegacao

---
+++ src/app/cartoes.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function CartoesScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Meus cartões</Text>
+    </View>
+  );
+}
---
+++ src/app/recarga.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function RecargaScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Tipo de recarga</Text>
+    </View>
+  );
+}
